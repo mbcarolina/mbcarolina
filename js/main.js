899cc0d0eb8144f3996204f111c0dc552fc9c190
Camila Marques <camilambdasilva@gmail.com>
2020-07-04 19:16:13 -0300

    arrumando a coluna da seção

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var banner = (name, banners) => {
     return '<div class="row mb-4">' +
-        '<div class="col-sm-2">' +
+        '<div class="col-sm-12">' +
         '<h1>' + name + '</h1>' +
         '</div>' +
         '</div>' +
